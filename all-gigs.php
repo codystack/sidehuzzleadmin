039a0345a1e865d746b8337b3ee2bfa8f2eb0789
codystack <ebuzzadvert@gmail.com>
2023-04-20 18:40:12 +0100

    users query added

---
+++ all-gigs.php
@@ -0,0 +1,9 @@
+<?php
+$page = "Gigs";
+include "./components/header.php";
+include "./components/navbar.php";
+include "./components/side-navbar.php";
+?>
+<main id="content" role="main" class="main">
+
+<?php include "./components/footer.php"; ?>
