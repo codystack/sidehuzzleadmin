c0f4bc44f150bb34577bd5c8d3ea8f8f49dec565
codystack <ebuzzadvert@gmail.com>
2023-04-20 18:16:34 +0100

    users query added

---
+++ 404.php
@@ -0,0 +1,40 @@
+<?php
+include "./components/header.php";
+?>
+<main>
+    <div class="content container-fluid mt-5 pt-5">
+      <div class="row justify-content-center align-items-sm-center py-sm-10">
+        <div class="col-9 col-sm-6 col-lg-4">
+          <div class="text-center text-sm-end me-sm-4 mb-5 mb-sm-0">
+            <img class="img-fluid" src="./assets/img/oc-thinking.svg" alt="Image Description" data-hs-theme-appearance="default">
+          </div>
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg-4 text-center text-sm-start">
+          <h1 class="display-1 mb-0 text-dark">404</h1>
+          <p class="lead">Sorry, the page you're looking for cannot be found.</p>
+          <a class="btn btn-dark" href="dashboard">Go back to dashboard</a>
+        </div>
+        <!-- End Col -->
+      </div>
+      <!-- End Row -->
+    </div>
+
+    <div style="position: absolute; bottom: 0; left: 0; right: 0; height: 3.5rem;">
+        <div class="row justify-content-between align-items-center">
+            <div class="col text-center">
+                <p class="fs-6 mb-0">&copy; <span class="d-none d-sm-inline-block"><script>document.write(new Date().getFullYear());</script> Sidehuzzle Canada. All rights reserved.</span></p>
+            </div>
+        </div>
+    </div>
+</main>   
+
+    <script src="assets/js/vendor.min.js"></script>
+
+    <script src="assets/vendor/chartjs-plugin-datalabels/dist/chartjs-plugin-datalabels.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/hs.theme-appearance-charts.js"></script>
+
+</body>
+</html>
