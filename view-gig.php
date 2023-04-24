37c4962d3183d7ead23d4fbb7f9a5d96ad2deb8c
codystack <ebuzzadvert@gmail.com>
2023-04-24 12:02:06 +0100

    top 5 gigs added

--- view-gig.php
+++ view-gig.php
@@ -35,8 +35,8 @@ include "./components/side-navbar.php";
                                 $gigDescription = $row['gigDescription'];
                                 $dateCreated = $row['dateCreated'];
                                 $registrationDate = strtotime($dateCreated);
-                                $gig_status = $row['status'];
-                                switch ($gig_status) {
+                                $status = $row['status'];
+                                switch ($status) {
                                     case "Approved";
                                         $class  = 'bg-soft-success';
                                         $text = 'text-success';
@@ -59,7 +59,7 @@ include "./components/side-navbar.php";
                     </div>
 
                     <h1 class="page-header-title"><?php echo $title; ?></h1>
-                    status: <span class="badge <?php echo $class; ?> <?php echo $text; ?>"><?php echo $gig_status; ?></span>
+                    <span class="badge <?php echo $class; ?> <?php echo $text; ?>"><?php echo $status; ?></span>
                 </div>
 
                 <div class="card mb-4">
