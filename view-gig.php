e13e0066029898b5b17a0734b928cc257900cdf9
codystack <ebuzzadvert@gmail.com>
2023-04-24 14:46:44 +0100

    top 5 gigs added

--- view-gig.php
+++ view-gig.php
@@ -17,7 +17,7 @@ include "./components/side-navbar.php";
 
                 <?php
                     $id = $_GET['id'];
-                    $select_query = "SELECT * FROM gigs INNER JOIN users ON gigs.userID = users.id WHERE gigs.id = '$id'";
+                    $select_query = "SELECT * FROM gigs WHERE id = '$id'";
                     $result = mysqli_query($conn, $select_query);
                     if (mysqli_num_rows($result) > 0) {
                             // output data of each row
